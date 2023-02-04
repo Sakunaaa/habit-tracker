09fa30e74c4a0ad1bfc8d96efdeca504876fd916
Sakunaaa <robertdaszkiewicz9@gmail.com>
2023-02-04 12:38:36 +0100

    Extract nav from Layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { HabitList } from '@/src/HabitList';
+import { Title } from '@mantine/core';
+
+// Wywietl date w Title aktualna (dzisiejsza)
 
 const IndexPage = () => {
 	return (
 		<main>
+			<Title></Title>
 			<HabitList />
 		</main>
 	);
--- src/Layout.js
+++ src/Layout.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Avatar, Flex, Text } from '@mantine/core';
 
+function Nav() {
+	return (
+		<Flex
+			mih={50}
+			justify="space-between"
+			align="center"
+			direction="row"
+			wrap="wrap"
+			p="md"
+		>
+			<span></span>
+			<Text fz="24px" fw="700">
+				Habit
+			</Text>
+			<Avatar
+				variant="filled"
+				radius="xl"
+				color="teal"
+				src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+			/>
+		</Flex>
+	);
+}
+
 export function Layout(props) {
 	return (
 		<div>
-			<Flex
-				mih={50}
-				justify="space-between"
-				align="center"
-				direction="row"
-				wrap="wrap"
-				p="md"
-			>
-				<span></span>
-				<Text fz="24px" fw="700">
-					Habit
-				</Text>
-				<Avatar
-					variant="filled"
-					radius="xl"
-					color="teal"
-					src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-				/>
-			</Flex>
+			<Nav />
 			{props.children}
 		</div>
 	);
