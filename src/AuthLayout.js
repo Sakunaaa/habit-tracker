088fdb30b61c473ebfef95c1fda876041ec27b8a
Sakunaaa <robertdaszkiewicz9@gmail.com>
2023-03-12 14:21:35 +0100

    Add redirect to /login

--- src/AuthLayout.js
+++ src/AuthLayout.js
@@ -1,10 +1,15 @@
 import { useEffect } from 'react';
 import { supabaseClient } from './supabaseClient';
+import { useRouter } from 'next/router';
 
 export function AuthLayout(props) {
+	const router = useRouter();
 	const getUser = async () => {
 		const result = await supabaseClient.auth.getUser();
-		console.log(result);
+		const isLoggedIn = result.data.user !== null;
+		if (!isLoggedIn) {
+			router.push('/login');
+		}
 	};
 	useEffect(() => {
 		getUser();
